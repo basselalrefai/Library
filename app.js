6b86e06eac4b1f10359d225e725d67e11da1725a
Ahmad Alrefai <bassel@basselalrefai.com>
2021-04-09 00:19:05 -0400

    Add function to render book to html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Variables
 const myLibrary = [];
+const libraryContainer = document.querySelector(".library-container");
 
 // Book Constructor
 function Book(title, author, numOfPages, readBefore) {
@@ -12,4 +13,23 @@ function Book(title, author, numOfPages, readBefore) {
 // Handler Functions
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function displayBook(book) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("card-container");
+    newDiv.innerHTML = `<div class="title-container">
+                    <h3 class="book-title">${book.title}</h3>
+                </div>
+                <div class="info-container">
+                    <p>Book author:</p>
+                    <p class="book-author">${book.author}</p>
+                    <p>Page number:</p>
+                    <p class="page-number">${book.numOfPages}</p>
+                    <label for="read">Read:</label>
+                    <input type="checkbox" name="read" id="read" ${
+                        book.readBefore ? 'checked="true"' : "null"
+                    } />
+                </div>`;
+    libraryContainer.appendChild(newDiv);
 }
